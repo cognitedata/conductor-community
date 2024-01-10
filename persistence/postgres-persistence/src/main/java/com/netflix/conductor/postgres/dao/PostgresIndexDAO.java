4ba13f8341fdebefe3aefe47f00b70fbb72d8686
Vincent <vincent.gay@cognite.com>
2024-01-10 12:48:51 +0100

    implement removeWorkflow and removeTask in PostgresIndexDAO

--- persistence/postgres-persistence/src/main/java/com/netflix/conductor/postgres/dao/PostgresIndexDAO.java
+++ persistence/postgres-persistence/src/main/java/com/netflix/conductor/postgres/dao/PostgresIndexDAO.java
@@ -12,6 +12,7 @@
 package com.netflix.conductor.postgres.dao;
 
 import java.sql.Timestamp;
+import java.time.Clock;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
@@ -199,7 +200,10 @@ public class PostgresIndexDAO extends PostgresBaseDAO implements IndexDAO {
 
     @Override
     public void removeWorkflow(String workflowId) {
-        logger.info("removeWorkflow is not supported for postgres indexing");
+        queryWithTransaction(
+            "DELETE FROM workflow_index WHERE workflow_id = ?",
+            q -> q.addParameter(workflowId).executeDelete()
+        );
     }
 
     @Override
@@ -222,7 +226,10 @@ public class PostgresIndexDAO extends PostgresBaseDAO implements IndexDAO {
 
     @Override
     public void removeTask(String workflowId, String taskId) {
-        logger.info("removeTask is not supported for postgres indexing");
+        queryWithTransaction(
+            "DELETE FROM task_index WHERE task_id = ?",
+            q -> q.addParameter(taskId).executeDelete()
+        );
     }
 
     @Override
